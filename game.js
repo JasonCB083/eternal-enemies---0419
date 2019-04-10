f97f69b8f4eaa262d8c3d13d29ffa70e06146f1c
JasonCB083 <jason.cllr@gmail.com>
2019-04-10 15:44:19 +0200

    player

--- game.js
+++ game.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function Game(canvas) {
+  this.player = null;
+  this.enemies = [];
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext('2d');
+
+};
+
+Game.prototype.startLoop = function() {
+
+  const loop = () => {
+    console.log('im in the loop');
+    this.clearCanvas();
+    this.updateCanvas();
+    this.drawCanvas();
+    
+
+    window.requestAnimationFrame(loop);
+  }
+  window.requestAnimationFrame(loop);
+}
+
+Game.prototype.clearCanvas = function() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+Game.prototype.updateCanvas = function() {
+
+}
+Game.prototype.drawCanvas = function() {
+
+}
+
